bc9b8c7e9d72540552a44f85f063c179b3219652
janus <emekamicro@gmail.com>
2016-03-14 09:23:19 +0100

    Added test function and filtered out noise

--- calendar.js
+++ calendar.js
@@ -10,20 +10,16 @@ day is {Number}
 */
 function Calendar(year, month, date){
     
-    if(arguments.length === 0) {
-        this.$P = new Date();
-        this.currentMonth = this.$P.getMonth();
-        this.currentDay = this.$P.getDay();
-        this.currentYear = this.$P.getFullYear();
-        this.currentDate = this.$P.getDate();
+    var cobject;
+    if(arguments.length === 3){
+        cobject = new Date(year, month, date);
     }
-    else if(arguments.length === 4){
-        this.$P = new Date(year, month, date);
-        this.currentMonth = this.$P.getMonth();
-        this.currentDay = this.$P.getDay();
-        this.currentYear = this.$P.getFullYear();
-        this.currentDate = this.$P.getDate();
-    }
+
+    cobject  = cobject || new Date();
+    this.currentMonth = cobject.getMonth();
+    this.currentDay = cobject.getDay();
+    this.currentYear = cobject.getFullYear();
+    this.currentDate = cobject.getDate();
           
 }
 
@@ -33,20 +29,13 @@ It increases month by 1
 Calendar.prototype.nextMonth = function() {
     var cMonth = this.currentMonth;
     if(cMonth >= 0 && cMonth < 11){
-        cMonth++;
-        this.currentMonth = cMonth;
+        this.currentMonth = ++cMonth;
         return this;
     }
-    else if(cMonth === 11){
-        console.log("This month is not possible");
-        var cYear = this.currentYear;
-        this.currentYear = cYear + 1;
+    if(cMonth === 11){
+        ++this.currentYear;
         this.currentMonth = 0;
         return this;
-    }
-    else{
-        console.log("This month is not possible");
-        return new Calendar();
     }
         
 };
@@ -56,21 +45,14 @@ It decreases month by 1
 */
 Calendar.prototype.prevMonth = function() {
     var cMonth = this.currentMonth;
-    console.log("This month is not possible");
     if (cMonth > 0 && cMonth <= 11) {
-        cMonth--;
-        this.currentMonth = cMonth;
+        this.currentMonth = --cMonth;
         return this;
     }
-    else if (cMonth === 0) {
-        var cYear = this.currentYear;
-        this.currentYear = cYear - 1;
+    if (cMonth === 0) {
+        --this.currentYear;
         this.currentMonth = 11;
         return this;
-    }
-    else {
-        console.log("This month is not possible");
-        return new Calendar();
     }
         
 };
@@ -79,9 +61,7 @@ Calendar.prototype.prevMonth = function() {
 It increases year by 1
 */
 Calendar.prototype.nextYear = function(){
-    console.log("This Year is not possible");
-    var cYear = this.currentYear;
-    this.currentYear = cYear + 1;
+    ++this.currentYear;
     return this;
     
 };
@@ -90,8 +70,7 @@ Calendar.prototype.nextYear = function(){
 It decreases year by 1
 */
 Calendar.prototype.prevYear = function()  {
-    var cYear = this.currentYear;
-    this.currentYear = cYear - 1;
+    --this.currentYear;
     return this;
 };
 
@@ -115,17 +94,15 @@ To calculate the day of the week for any calendar date
 */
 function zeller(year, month , day) {
     if ((month < 1) || (month > 12)) {
-        console.log("Impossible month");
-        return null;
+        throw new Error("Bad month" + month);
     }
     if ((day < 1 ) || (day > 31)) {
-        console.log("Imposible day");
-        return null;
+        throw new Error("Bad day " + day);
     }
     var ttable = [0,3,2,5,0,3,5,1,4,6,2,4];
     var ntable = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     if (month < 3){
-        year = year - 1;
+        --year;
     }
     var nw = (year + Math.floor(year/4) - Math.floor(year/100) + Math.floor(year/400) + ttable[month - 1] + day) % 7;
     return ntable[nw];
@@ -136,6 +113,7 @@ function zeller(year, month , day) {
 Private function for render month's days
 Returns an Array of {Numbers} and {Null}
 */
+/*
 function render(that) {
     var layout = [];
     var firstDateOfMonth = zeller( that.currentYear, that.currentMonth + 1, 1);
@@ -144,31 +122,90 @@ function render(that) {
         layout.push(null);
         numOfVoids = numOfVoids - 1;
     }
-    var numOfDays = Calendar.monthDays(that.currentMonth ,that.currentYear);
+    var numOfDays = that.monthDays(that.currentMonth ,that.currentYear);
     for(var i = 1 ;  i <= numOfDays ; i++) {
         layout.push(i);
     }
     return layout;
 }
 
-Calendar.weekName = function (week) {
-    return ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][week];
-}
-
-Calendar.monthName = function (month) {
-    return ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][month];
+*/
+Calendar.prototype.dayName = function () {
+    return ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][this.currentDay];
 };
 
+/*
+*This is zero-based, so 0 -> January
+*/
+Calendar.prototype.monthName = function () {
+    return ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][this.currentMonth];
+};
 
-Calendar.monthDays = function(month, year) {
-    return [31, leapYear(year)? 29 : 28, 31, 30, 31, 30 , 31, 31, 30, 31, 30, 31 ][month];
-}
+/*
+*This is zero-based, so 0 -> 31
+*/
 
+Calendar.prototype.monthDays = function(month, year) {
+    return [31, leapYear(this.currentYear)? 29 : 28, 31, 30, 31, 30 , 31, 31, 30, 31, 30, 31 ][this.currentMonth];
+};
+
+/*
+Private function for render month's days
+Returns an Array of {Numbers} and {Null}
+*/
 
 Calendar.prototype.render = function() {
-    return render(this);
-    //To Do print the result here
     
-    
+    var layout = [];
+    var firstDateOfMonth = zeller( this.currentYear, this.currentMonth + 1, 1);
+    var numOfVoids = matchWeekNumber(firstDateOfMonth);
+    while ( numOfVoids > 0) {
+        layout.push(null);
+        --numOfVoids;
+    }
+    var numOfDays = this.monthDays(this.currentMonth ,this.currentYear);
+    for(var i = 1 ;  i <= numOfDays ; i++) {
+        layout.push(i);
+    }
+    return layout;
+    //To Do print the result here   
+};
+
+/*
+* Simple unit test
+*
+*
+*/
+
+function test(computed, given) {
+    var clen = computed.length;
+    var glen = given.length;
+    if ( clen !== glen) {
+        throw new Error("Test failed: length not equal " + clen + " " + glen);
+    }
+    for(var i = 0; i < clen; i++) {
+        if (computed[i] !== given[i]) {
+            throw new Error("Test failed : Elements not same at " + i)
+        }
+            
+    }
+    return "Test Passed";
 }
+
+var testApril2017 = [null, null, null, null, null, null, 1,  2,  3, 4, 5,  6, 7, 8,  9, 10, 11 , 12, 13, 14, 15, 16, 17, 18, 19,20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
+
+var testMarch2016 = [null, null, 1,  2,  3, 4, 5,  6, 7, 8,  9, 10, 11 , 12, 13, 14, 15, 16, 17, 18, 19,20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+var testMarch2017 = [null, null, null, 1,  2,  3, 4, 5,  6, 7, 8,  9, 10, 11 , 12, 13, 14, 15, 16, 17, 18, 19,20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+var testMarch2015 = [1,  2,  3, 4, 5,  6, 7, 8,  9, 10, 11 , 12, 13, 14, 15, 16, 17, 18, 19,20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+
+test(new Calendar(2017, 2, 14).nextMonth().render(), testApril2017);
+     
+test(new Calendar().render(), testMarch2016);
+
+test(new Calendar().nextYear().render(), testMarch2017);
+
+test(new Calendar().prevYear().render(), testMarch2015);
 
